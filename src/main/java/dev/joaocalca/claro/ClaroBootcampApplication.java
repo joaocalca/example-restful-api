82cfb965fc63ea9ca766682178da24987a793b85
joaocalca <desenvolvimento.joaocalca@gmail.com>
2024-10-25 15:40:20 -0300

    Initial commit

---
+++ src/main/java/dev/joaocalca/claro/ClaroBootcampApplication.java
@@ -0,0 +1,13 @@
+package dev.joaocalca.claro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroBootcampApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaocalca/claro/ClaroBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaocalca.claro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
